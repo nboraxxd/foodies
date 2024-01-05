29dfb25c89a8e1e83bd7e3800e37ec329bce6a4b
bora <nhatbaoxxd@gmail.com>
2024-01-05 10:32:31 +0700

    Add react-wrap-balancer

--- app/meals/page.tsx
+++ app/meals/page.tsx
@@ -2,15 +2,20 @@ import Link from 'next/link'
 
 import { MealsGrid } from '@/components/meals'
 import classes from './page.module.css'
+import Balancer from 'react-wrap-balancer'
 
 export default function MealsPage() {
   return (
     <>
       <header className={classes.header}>
         <h1>
-          Delicious meals, created <span className={classes.highlight}>by you</span>
+          <Balancer>
+            Delicious meals, created <span className={classes.highlight}>by you</span>
+          </Balancer>
         </h1>
-        <p>Choose your favorite recipe and cook it yourself. It is easy and fun</p>
+        <p>
+        Choose your favorite recipe and cook it yourself. It is easy and fun
+        </p>
         <p className={classes.cta}>
           <Link href="/meals/share">Share Your Favorite Recipe</Link>
         </p>
