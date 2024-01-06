5175e5f67bfc5edae6870158dd4b233440360fff
bora <nhatbaoxxd@gmail.com>
2024-01-06 15:01:53 +0700

    Adding Server Side Input Validation

--- lib/actions.ts
+++ lib/actions.ts
@@ -5,6 +5,10 @@ import { Meal } from '@/types/meal.type'
 import { saveMeal } from '@/lib/meals'
 import { redirect } from 'next/navigation'
 
+function isInvalidText(text: string): boolean {
+  return !text || text.trim().length === 0
+}
+
 export async function shareMeal(formData: FormData) {
   const meal: Omit<Meal, 'id' | 'image'> & { image: File } = {
     title: formData.get('title') as string,
@@ -14,6 +18,19 @@ export async function shareMeal(formData: FormData) {
     creator: formData.get('name') as string,
     creator_email: formData.get('email') as string,
     slug: slugify(formData.get('title') as string, { lower: true }),
+  }
+
+  if (
+    isInvalidText(meal.title) ||
+    isInvalidText(meal.summary) ||
+    isInvalidText(meal.instructions) ||
+    isInvalidText(meal.creator) ||
+    isInvalidText(meal.creator_email) ||
+    !meal.creator_email.includes('@') ||
+    !meal.image ||
+    meal.image.size === 0
+  ) {
+    throw new Error('Invalid input')
   }
 
   await saveMeal(meal)
